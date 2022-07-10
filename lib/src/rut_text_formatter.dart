53126f79b468ef6d9d25103ab9be82419f7a4c1f
Sebastian Manriquez <smanriquez@simplepark.cl>
2022-07-10 18:34:06 -0400

    feat: rut text formatter

---
+++ lib/src/rut_text_formatter.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/services.dart';
+import 'package:rut_utils/src/rut_utils.dart';
+
+import 'constants/constants.dart';
+
+class RutFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.selection.baseOffset == 0) {
+      return newValue;
+    }
+    var rut = deFormatRut(newValue.text);
+    var verificationDigit = rut.substring(rut.length - 1);
+
+    if (!kAllowedRutVerificationDigitChars.contains(verificationDigit)) {
+      return oldValue;
+    }
+
+    var finalValue =
+        rut.length < 8 ? shortVersionRutFormat(rut) : formatRut(rut);
+
+    return newValue.copyWith(
+        text: finalValue.toUpperCase(),
+        selection: TextSelection.collapsed(offset: finalValue.length));
+  }
+}
